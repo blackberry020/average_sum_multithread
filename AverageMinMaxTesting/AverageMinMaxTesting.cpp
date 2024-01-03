434d7ad6ef1fac7fa0a76e52176bff3a246512e1
blackberry020 <ilonafrolova00@gmail.com>
2024-01-03 17:30:11 +0300

    upd tests

--- AverageMinMaxTesting/AverageMinMaxTesting.cpp
+++ AverageMinMaxTesting/AverageMinMaxTesting.cpp
@@ -5,7 +5,7 @@
 
 #include <vector>
 
-#include "..\lab2_sum\functions.h"
+//#include "..\lab2_sum\functions.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -18,9 +18,11 @@ namespace AverageMinMaxTesting
 		TEST_METHOD(TestAverage)
 		{
 			std::vector<int> a = {1, 2, 3, 4};
-			Array<int> array(a);
+			//Array<int> array(a);
 
-			array.getSize();
+			//int dop = findAverageInArray(array);
+
+			Assert::AreEqual(2, a[1]);
 		}
 	};
 }
